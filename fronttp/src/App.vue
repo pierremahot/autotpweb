20fa26310cd060484b809a21a20ebcbda21de5d3
pierremahot <pierre.mahot@orange.fr>
2018-09-24 18:49:36 +0200

    beautify

--- fronttp/src/App.vue
+++ fronttp/src/App.vue
@@ -6,25 +6,25 @@
 </template>
 
 <script>
-import dropzone from './components/dropzone.vue'
-import myform from './components/myform.vue'
-
-export default {
-  name: 'app',
-  components: {
-    dropzone,
-    myform
+  import dropzone from './components/dropzone.vue'
+  import myform from './components/myform.vue'
+  
+  export default {
+    name: 'app',
+    components: {
+      dropzone,
+      myform
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
